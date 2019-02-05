dbf921a813acb3763065a7c645edb0d0c17c30a8
Denis Sablin <denis.sablin@arcadia.spb.ru>
2019-02-05 19:36:44 +0300

    Exercise1: second expression fixed

--- QaProgrammingCourse/QaProgrammingCourse/Exercise1.cs
+++ QaProgrammingCourse/QaProgrammingCourse/Exercise1.cs
@@ -11,7 +11,7 @@ namespace QaProgrammingCourse
                 z = int.Parse(Console.ReadLine());
 
             Console.WriteLine($"First expression result: {(x + y) * (Math.Pow(2, z) + 1)}");
-            Console.WriteLine($"Second expression result: {(x / z - 1) * Math.Sqrt(y)}");
+            Console.WriteLine($"Second expression result: {(x % z - 1) * Math.Sqrt(y)}");
             Console.WriteLine($"Third expression result: {(x * y + y * z) / Math.Pow(3, z)}");
             Console.ReadKey();
         }
