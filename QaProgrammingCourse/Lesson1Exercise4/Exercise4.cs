94b587cbc08b302d591e8867b9e3a91d46067d96
Denis Sablin <denis.sablin@arcadia.spb.ru>
2019-02-06 15:07:49 +0300

    Exercise4 style updated.

--- QaProgrammingCourse/Lesson1Exercise4/Exercise4.cs
+++ QaProgrammingCourse/Lesson1Exercise4/Exercise4.cs
@@ -6,7 +6,8 @@ namespace Lesson1Exercise4
     {
         static void Main()
         {
-            string xx = Console.ReadLine(), yy = Console.ReadLine();
+            string xx = Console.ReadLine(), 
+                   yy = Console.ReadLine();
             char n = Convert.ToChar(Console.ReadLine());
             string combinedRow = string.Concat(xx, yy);
             Console.WriteLine($"Index of last {n} in {combinedRow}: " + combinedRow.LastIndexOf(n));
